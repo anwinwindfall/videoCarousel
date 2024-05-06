5c36e6e50326fe0f13cac796659930e41f7f4d9b
anwinwindfall <anwina@windfallproductions.org>
2024-05-06 12:45:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App({ moduleData }) {
+  // eslint-disable-next-line no-console
+  // console.log(
+  //   'all of your data typically accessed via the "module" keyword in HubL is available as JSON here!',
+  //   moduleData,
+  // );
+  return (
+    <></>
+  );
+}
+
+export default App;
