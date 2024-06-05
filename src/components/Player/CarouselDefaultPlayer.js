6841e827ef4fde05e3825beccccbb0e3073421fb
anwinwindfall <anwina@windfallproductions.org>
2024-06-05 14:32:07 +0530

    vimeo app configured

--- src/components/Player/CarouselDefaultPlayer.js
+++ src/components/Player/CarouselDefaultPlayer.js
@@ -4,14 +4,45 @@ import './CarouselDefaultPlayer.scss'
 import Shimmer from '../Shimmer';
 
 
-const CarouselDefaultPlayer = ({ type, url, poster, title}) => {
-  const [isLoaded, setIsLoaded] = useState(false);
+const CarouselDefaultPlayer = ({ type, url, poster, title }) => {
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [videoBlobUrl, setVideoBlobUrl] = useState()
+  const [loading, setLoading] = useState(false)
   const videoRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
   const [volume, setVolume] = useState(1);
-  const [currentTime, setCurrentTime] = useState(0);
+  const [metaDataLoaded, setMetaDataLoaded] = useState(false);
   const [duration, setDuration] = useState(0);
+  const regex = /\/(\d+)\.m3u8/;
+  const match = url.match(regex);
+  const videoId = match[1]
+
+  const fetchVimeoM3U8Url = async (videoId) => {
+    const response = await fetch(`https://api.vimeo.com/videos/${videoId}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_VIMEO_TOKEN}`
+      }
+    });
+    const data = await response.json();
+    // Assuming the .m3u8 URL is under 'files' in the API response
+    const m3u8File = data.files.find(file => file.rendition === '1080p');
+    return m3u8File ? m3u8File.link : null;
+  };
+  const fetchVideoUrl = async () => {
+    setLoading(true);
+    try {
+      const url = await fetchVimeoM3U8Url(videoId);
+      const response = await fetch(url);
+      const blob = await response.blob();
+      const objectUrl = URL.createObjectURL(blob);
+      setVideoBlobUrl(objectUrl);
+    } catch (error) {
+      console.error('Error fetching video URL:', error);
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
     const handleFullScreenChange = () => {
       const video = videoRef.current;
@@ -34,21 +65,41 @@ const CarouselDefaultPlayer = ({ type, url, poster, title}) => {
     };
   }, []);
 
-  const handlePlayPause = () => {
+  const handlePlay = async () => {
     const video = videoRef.current;
+    if (!document.fullscreenElement) {
+      handleFullScreen();
+    }
+    if (videoRef.current) {
+      const videos = document.querySelectorAll('video');
+      videos.forEach(video => {
+        if (video !== videoRef.current && !video.paused) {
+          video.pause();
+        }
+      });
+    }
+    if (!videoBlobUrl) {
+      await fetchVideoUrl()
+    }
     if (video.paused) {
-      if (!document.fullscreenElement) {
-        handleFullScreen();
-      }
-      video.play();
-      setIsPlaying(true);
+        video.play();
+        setIsPlaying(true);
       
-      
+
     } else {
       video.pause();
       setIsPlaying(false);
     }
   };
+  function handlePause() {
+    const video = videoRef.current;
+    if (!video.paused) {
+      video.pause()
+      setIsPlaying(false)
+    }
+  }
+
+
 
   const handleMute = () => {
     const video = videoRef.current;
@@ -76,7 +127,7 @@ const CarouselDefaultPlayer = ({ type, url, poster, title}) => {
 
   const handleLoadedMetadata = () => {
     const video = videoRef.current;
-    setDuration(Math.ceil(video.duration/60));
+    setDuration(Math.ceil(video.duration / 60));
     setIsLoaded(true)
   };
 
@@ -93,62 +144,71 @@ const CarouselDefaultPlayer = ({ type, url, poster, title}) => {
     }
   };
   //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  function handlePlayerReady(){
+  function handlePlayerReady() {
     setIsLoaded(true)
   }
-  function handleWidth(type){
-    if (type=='default') {
+  function handleWidth(type) {
+    if (type == 'default') {
       return '375px'
     }
-    else{
+    else {
       return '241px'
     }
   }
-  function handlHeight(type){
-    if (type=='default') {
+  function handlHeight(type) {
+    if (type == 'default') {
       return '205px'
     }
-    else{
+    else {
       return '410px'
     }
   }
+  const videos = document.querySelectorAll('video');
+
   return (
     <div className={`${type == "default" ? 'Video-player-class-wrap' : 'Video-player-class-story-wrap'}`}>
-      {!isLoaded&&<Shimmer type={type}/>}
-    <video ref={videoRef}
-    onLoadedData={handlePlayerReady}
-    onLoadedMetadata={handleLoadedMetadata}
-    width={handleWidth(type)}
-    height={handlHeight(type)}
-    poster={poster?.src}
-    disableRemotePlayback 
-    controls={false}
-    muted={isMuted}
-    disablepictureinpicture controlslist="nodownload noplaybackrate"
-    src={url}
-    preload='auto'
-    className={`${type == "default" ? 'Video-player-class' : 'Video-player-class-story'}`}>
-  </video>
-  <div className='controls'>
-  {isLoaded&&<button onClick={handlePlayPause} className='btn-play'>
-          {isPlaying ? <img src='https://upload.wikimedia.org/wikipedia/commons/d/dc/Pause_Button_icon_Apple.svg'/> : <img src='https://143223961.fs1.hubspotusercontent-eu1.net/hubfs/143223961/raw_assets/public/Assets/Eucharistic%20Revival/play.svg'/>}
+      {/* {!isLoaded&&<Shimmer type={type}/>} */}
+      {loading && (
+        <span
+          className='video_loader'
+        >
+        </span>
+      )}
+
+      <div>
+        <video ref={videoRef}
+          onLoadedData={handlePlayerReady}
+          onLoadedMetadata={handleLoadedMetadata}
+          onCanPlay={() => setMetaDataLoaded(true)}
+          width={handleWidth(type)}
+          height={handlHeight(type)}
+          poster={!loading && poster?.src}
+          disableRemotePlayback
+          controls={false}
+          disablepictureinpicture controlslist="nodownload noplaybackrate"
+          preload='auto'
+          src={videoBlobUrl}
+          className={`${type == "default" ? 'Video-player-class' : 'Video-player-class-story'}`}>
+        </video>
+      </div>
+
+
+      <div className='player_controls'>
+        {isLoaded && !loading && <button className='btn-play'>
+          {console.log(isPlaying, isLoaded, "is loaded")}
+          {isPlaying ? <img onClick={handlePause} className='pause_btn' src='https://upload.wikimedia.org/wikipedia/commons/d/dc/Pause_Button_icon_Apple.svg' /> : <img onClick={handlePlay} src='https://143223961.fs1.hubspotusercontent-eu1.net/hubfs/143223961/raw_assets/public/Assets/Eucharistic%20Revival/play.svg' />}
         </button>}
-  </div>
-  {isLoaded&&type=='default'&&<div className='title'>
-     <p>{`${duration} mins watch`}</p>
-      <h1>{title&&title}</h1>
-  </div>}
-      
+        <div onClick={handleFullScreen} className='btn-fullscreen'>
+          <img src='https://143223961.fs1.hubspotusercontent-eu1.net/hubfs/143223961/raw_assets/public/Assets/Eucharistic%20Revival/fullscreenIcon.svg' />
         </div>
+      </div>
+      {isLoaded && type == 'default' && <div className='title'>
+        {metaDataLoaded && <p>{`${duration} mins watch`}</p>}
+        <h1>{title && title}</h1>
+      </div>}
+
+    </div>
 
   )
-  }
+}
 export default CarouselDefaultPlayer
-
-
-
-
-
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,34 @@ const HubSpotAutoUploadPlugin = require('@hubspot/webpack-cms-plugins/HubSpotAut
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+const fs = require('fs');
+
+
+// Determine the appropriate .env file based on the environment
+const envPath = path.resolve(__dirname, `.env.${process.env.NODE_ENV}`);
+const fallbackEnvPath = path.resolve(__dirname, '.env');
+const envFilePath = fs.existsSync(envPath) ? envPath : fallbackEnvPath;
+
+// Read and parse the .env file manually
+const envFileContent = fs.readFileSync(envFilePath, 'utf8');
+const envVariables = envFileContent.split('\n').reduce((acc, line) => {
+  const [key, value] = line.split('=');
+  if (key && value) {
+    acc[key.trim()] = value.trim();
+  }
+  return acc;
+}, {});
+
+// Set environment variables for EnvironmentPlugin
+const envKeys = Object.keys(envVariables).reduce((prev, next) => {
+  prev[next] = envVariables[next];
+  return prev;
+}, {});
+
+// Add NODE_ENV
+const isDevelopment = process.env.NODE_ENV !== 'production';
+envKeys['process.env.NODE_ENV'] = JSON.stringify(isDevelopment ? 'development' : 'production');
 
 
 const hubspotConfig = ({ portal, autoupload } = {}) => {
@@ -67,6 +95,7 @@ const hubspotConfig = ({ portal, autoupload } = {}) => {
           },
         ],
       }),
+      new webpack.EnvironmentPlugin(envKeys),
     ],
   };
 };
